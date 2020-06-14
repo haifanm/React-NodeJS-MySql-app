e35f497ff569ec11faca8f20de24544533abc078
haifanm <haifa-naim@hotmail.com>
2020-06-14 14:13:02 +0300

    react app build

---
+++ react-app/build/precache-manifest.361f266a9238b87520104c6320aadc60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cade340574da9cb792519eb5788d3ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687570fb53e7b4c164b6",
+    "url": "/static/css/main.a1fa57c3.chunk.css"
+  },
+  {
+    "revision": "56cced6865ef456fe077",
+    "url": "/static/js/2.05fc42e4.chunk.js"
+  },
+  {
+    "revision": "baa0dc55e0009f30acf6b5f9095cd449",
+    "url": "/static/js/2.05fc42e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687570fb53e7b4c164b6",
+    "url": "/static/js/main.6f2c35e0.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
